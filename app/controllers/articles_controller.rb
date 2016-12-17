3f8288349d289bc7eed21af155be2f066261b6bf
Richard Nguyen <cuongnt1987@live.com>
2016-12-18 00:31:46 +0700

    Completed getting start with rails

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,13 @@
 class ArticlesController < ApplicationController
 
+  http_basic_authenticate_with name: 'cuongnt', password: 'thanhlong', except: [:index, :show]
+
   def index
     @articles = Article.all
   end
 
   def new
+    @article = Article.new
   end
 
   def create
@@ -32,6 +35,13 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find params[:id]
+  end
+
+  def destroy
+    @article = Article.find params[:id]
+    @article.destroy
+
+    redirect_to articles_path
   end
 
   private
